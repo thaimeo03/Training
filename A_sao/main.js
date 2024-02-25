c1d6a845cbfb2c7845e2a3fdbfc40396d9c3fc0c
thaimeo03 <thaitran15072003@gmail.com>
2024-02-25 15:20:32 +0700

    Fix A stat

--- A_sao/main.js
+++ A_sao/main.js
@@ -1,35 +1,57 @@
 var BoatNode = /** @class */ (function () {
-    function BoatNode(value, cost, parent) {
+    function BoatNode(value, cost, totalCost, parent) {
         this.value = value;
         this.cost = cost;
         this.parent = parent;
+        this.totalCost = totalCost;
     }
     return BoatNode;
 }());
 var Game = /** @class */ (function () {
     function Game() {
-        this.L = [new BoatNode([3, 3, 1], 0)];
+        this.L = [new BoatNode([3, 3, 1], 0, 0)];
         this.step = [[0, 1], [1, 0], [1, 1], [0, 2], [2, 0]];
         this.visit = {}; // Can use map or dictionary (i used dictionary instead)
         this.visit["".concat(this.L[0].value)] = true;
     }
+    Game.prototype.conditionOppSide = function (u) {
+        var oppSide = [3 - u.value[0], 3 - u.value[1]];
+        return (oppSide[0] >= oppSide[1] || oppSide[0] === 0 || oppSide[0] === 3) && (u.value[0] >= u.value[1] || u.value[0] === 0 || u.value[0] === 3);
+    };
+    Game.prototype.conditionStartSide = function (u, r) {
+        if (u.value[0] >= r[0] && u.value[1] >= r[1]) {
+            var newNode = [u.value[0] - r[0], u.value[1] - r[1], 0];
+            var newState = new BoatNode(newNode, u.cost + 1, 0, u); // g(v) = k(u, v) + 1 = cost of parent + 1
+            var isOppSide = this.conditionOppSide(newState);
+            return isOppSide ? newState : null;
+        }
+        return null;
+    };
+    Game.prototype.conditionEndSide = function (u, r) {
+        if (u.value[0] + r[0] <= 3 && u.value[1] + r[1] <= 3) {
+            var newNode = [u.value[0] + r[0], u.value[1] + r[1], 1];
+            var newState = new BoatNode(newNode, u.cost + 1, 0, u); // g(v) = k(u, v) + 1 = cost of parent + 1
+            var isOppSide = this.conditionOppSide(newState);
+            return isOppSide ? newState : null;
+        }
+        return null;
+    };
     Game.prototype.generatePossibleMoves = function (u) {
+        var _this = this;
         var v = [];
         if (u.value[2] === 1) {
             this.step.forEach(function (r) {
-                if (u.value[0] >= r[0] && u.value[1] >= r[1]) {
-                    var newNode = [u.value[0] - r[0], u.value[1] - r[1], 0];
-                    var newState = new BoatNode(newNode, u.cost + 1, u);
-                    v.push(newState);
+                var newBoatNode = _this.conditionStartSide(u, r);
+                if (newBoatNode) {
+                    v.push(newBoatNode);
                 }
             });
         }
         else {
             this.step.forEach(function (r) {
-                if (u.value[0] + r[0] <= 3 && u.value[1] + r[1] <= 3) {
-                    var newNode = [u.value[0] + r[0], u.value[1] + r[1], 1];
-                    var newState = new BoatNode(newNode, u.cost + 1, u);
-                    v.push(newState);
+                var newBoatNode = _this.conditionEndSide(u, r);
+                if (newBoatNode) {
+                    v.push(newBoatNode);
                 }
             });
         }
@@ -41,7 +63,7 @@ var Game = /** @class */ (function () {
     Game.prototype.handleF = function (boatNodes) {
         var h = this.handleH(boatNodes);
         for (var i = 0; i < boatNodes.length; i++) {
-            boatNodes[i].cost += h[i];
+            boatNodes[i].totalCost = boatNodes[i].cost + h[i];
         }
         // h.forEach(e => console.log(e))
         // arr.forEach(e => console.log(e.cost))
@@ -50,18 +72,18 @@ var Game = /** @class */ (function () {
     Game.prototype.updateQueue = function (v) {
         for (var _i = 0, v_1 = v; _i < v_1.length; _i++) {
             var x = v_1[_i];
-            var oppSide = [3 - x.value[0], 3 - x.value[1]];
-            if ((oppSide[0] >= oppSide[1] || oppSide[0] === 0 || oppSide[0] === 3) && (x.value[0] >= x.value[1] || x.value[0] === 0 || x.value[0] === 3) && !this.visit["".concat(x.value)]) {
+            if (!this.visit["".concat(x.value)]) {
                 this.L.push(x);
                 this.visit["".concat(x.value)] = true;
             }
         }
         // Sort L by cost
-        this.L.sort(function (a, b) { return a.cost - b.cost; });
+        this.L.sort(function (a, b) { return a.totalCost - b.totalCost; });
         console.log(this.L.map(function (boatNode) {
             return {
                 boatNode: boatNode.value,
-                cost: boatNode.cost // cost by F function
+                cost: boatNode.cost,
+                totalCost: boatNode.totalCost
             };
         }));
     };
--- A_sao/main.ts
+++ A_sao/main.ts
@@ -1,17 +1,19 @@
 class BoatNode {
   value: number[];
-  cost: number;
+  cost: number; // g(v) function
+  totalCost: number; // f(v) function
   parent?: BoatNode;
 
-  constructor(value: number[], cost: number, parent?: BoatNode) {
+  constructor(value: number[], cost: number, totalCost: number,parent?: BoatNode) {
     this.value = value;
     this.cost = cost;
     this.parent = parent;
+    this.totalCost = totalCost
   }
 }
 
 class Game {
-  private L: BoatNode[] = [new BoatNode([3, 3, 1], 0)];
+  private L: BoatNode[] = [new BoatNode([3, 3, 1], 0, 0)];
   private step = [[0, 1], [1, 0], [1, 1], [0, 2], [2, 0]];
   private visit: {[key: string]: boolean} = {}; // Can use map or dictionary (i used dictionary instead)
   private goalState: BoatNode | undefined;
@@ -20,26 +22,50 @@ class Game {
     this.visit[`${this.L[0].value}`] = true;
   }
 
+  private conditionOppSide(u: BoatNode): boolean {
+    const oppSide: number[] = [3 - u.value[0], 3 - u.value[1]]
+    return (oppSide[0] >= oppSide[1] || oppSide[0] === 0 || oppSide[0] === 3) && (u.value[0] >= u.value[1] || u.value[0] === 0 || u.value[0] === 3)
+  }
+
+  private conditionStartSide(u: BoatNode, r: number[]): BoatNode | null {
+    if(u.value[0] >= r[0] && u.value[1] >= r[1]) {
+      const newNode = [u.value[0] - r[0], u.value[1] - r[1], 0]
+      const newState = new BoatNode(newNode, u.cost + 1, 0, u); // g(v) = k(u, v) + 1 = cost of parent + 1
+      const isOppSide = this.conditionOppSide(newState)
+      return isOppSide ? newState : null
+    }
+    return null
+  }
+
+  private conditionEndSide(u: BoatNode, r: number[]): BoatNode | null {
+    if(u.value[0] + r[0] <= 3 && u.value[1] + r[1] <= 3) {
+      const newNode = [u.value[0] + r[0], u.value[1] + r[1], 1];
+      const newState = new BoatNode(newNode, u.cost + 1, 0, u) // g(v) = k(u, v) + 1 = cost of parent + 1
+      const isOppSide = this.conditionOppSide(newState)
+      return isOppSide ? newState : null
+    }
+    return null
+  }
+
   private generatePossibleMoves(u: BoatNode): BoatNode[] {
     const v: BoatNode[] = [];
     if(u.value[2] === 1) {
       this.step.forEach((r) => {
-        if(u.value[0] >= r[0] && u.value[1] >= r[1]) {
-          const newNode = [u.value[0] - r[0], u.value[1] - r[1], 0];
-          const newState = new BoatNode(newNode, u.cost + 1, u);
-          v.push(newState);
+        const newBoatNode = this.conditionStartSide(u, r);
+        if(newBoatNode) {
+          v.push(newBoatNode);
         }
       });
     }
     else {
       this.step.forEach((r) => {
-        if(u.value[0] + r[0] <= 3 && u.value[1] + r[1] <= 3) {
-          const newNode = [u.value[0] + r[0], u.value[1] + r[1], 1];
-          const newState = new BoatNode(newNode, u.cost + 1, u);
-          v.push(newState);
+        const newBoatNode = this.conditionEndSide(u, r);
+        if(newBoatNode) {
+          v.push(newBoatNode);
         }
       });
     }
+
     return this.handleF(v);
   }
 
@@ -50,7 +76,7 @@ class Game {
   private handleF(boatNodes: BoatNode[]): BoatNode[] {
     const h = this.handleH(boatNodes);
     for(let i = 0; i < boatNodes.length; i++) {
-      boatNodes[i].cost += h[i];
+      boatNodes[i].totalCost = boatNodes[i].cost + h[i];
     }
     // h.forEach(e => console.log(e))
     // arr.forEach(e => console.log(e.cost))
@@ -59,18 +85,18 @@ class Game {
 
   private updateQueue(v: BoatNode[]): void {
     for(const x of v) {
-      const oppSide: number[] = [3 - x.value[0], 3 - x.value[1]]
-      if((oppSide[0] >= oppSide[1] || oppSide[0] === 0 || oppSide[0] === 3) && (x.value[0] >= x.value[1] || x.value[0] === 0 || x.value[0] === 3) && !this.visit[`${x.value}`]) {
+      if(!this.visit[`${x.value}`]) {
         this.L.push(x)
         this.visit[`${x.value}`] = true
       }
     }
     // Sort L by cost
-    this.L.sort((a, b) => a.cost - b.cost);
+    this.L.sort((a, b) => a.totalCost - b.totalCost);
     console.log(this.L.map(boatNode => {
       return {
         boatNode: boatNode.value,
-        cost: boatNode.cost // cost by F function
+        cost: boatNode.cost,
+        totalCost: boatNode.totalCost
       }
     }))
   }
